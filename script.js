57b1e9a14fe8c2e42d0f983187b50fd5f7aa5716
tatsumi04 <tatumi-o-04@outlook.jp>
2022-07-24 23:10:26 +0900

    イベント発火 基礎編
    mouseover
    mouseout
    click

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
+window.addEventListener('load' , function(){
+    const pullDownButton = document.getElementById("lists");
+
+    pullDownButton.addEventListener("mouseover" , function(){
+        console.log("乗る");
+    });
+
+    pullDownButton.addEventListener("mouseout" , function(){
+        console.log("外れる");
+    });
+
+    pullDownButton.addEventListener("click" , function(){
+        console.log("クリック");
+    })
+})
