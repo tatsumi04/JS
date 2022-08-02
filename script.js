1745c8f603cdd953c360a25aa290d393f18876ab
tatsumi04 <tatumi-o-04@outlook.jp>
2022-08-02 21:06:27 +0900

    インラインスタイル編

--- script.js
+++ script.js
@@ -12,4 +12,16 @@ window.addEventListener('load' , function(){
     pullDownButton.addEventListener("click" , function(){
         console.log("クリック");
     })
+
+    pullDownButton.addEventListener("mouseover" , function(){
+        pullDownButton.setAttribute("style" , "background-color:blue;")
+    })
+
+    pullDownButton.addEventListener("mouseout" , function(){
+        pullDownButton.setAttribute("style" , "background-color:#FF6666;")
+    })
+
+    pullDownButton.addEventListener("click" , function(){
+        pullDownButton.setAttribute("style" , "background-color:green;")
+    })
 })
