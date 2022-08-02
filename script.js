ed2b82b5c29b5ad31f7d6a3066a291780fcb67be
tatsumi04 <tatumi-o-04@outlook.jp>
2022-08-02 22:15:14 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.addEventListener('load' , function(){
     const pullDownButton = document.getElementById("lists");
+    const pullDownParents = document.getElementById("pull-down");
 
     pullDownButton.addEventListener("mouseover" , function(){
         console.log("乗る");
@@ -14,14 +15,24 @@ window.addEventListener('load' , function(){
     })
 
     pullDownButton.addEventListener("mouseover" , function(){
-        pullDownButton.setAttribute("style" , "background-color:blue;")
+        this.setAttribute("style" , "background-color:blue;");
     })
 
     pullDownButton.addEventListener("mouseout" , function(){
-        pullDownButton.setAttribute("style" , "background-color:#FF6666;")
+        this.setAttribute("style" , "background-color:#FF6666;");
     })
 
     pullDownButton.addEventListener("click" , function(){
-        pullDownButton.setAttribute("style" , "background-color:green;")
+        this.setAttribute("style" , "background-color:green;");
+    })
+
+    pullDownButton.addEventListener("click" , function(){
+        if(pullDownParents.getAttribute("style") === "display:block;"){
+            pullDownParents.removeAttribute("style" , "display:block;");
+            console.log("非表示");
+        }else{
+            pullDownParents.setAttribute("style" , "display:block;");
+            console.log("表示");
+        }
     })
 })
